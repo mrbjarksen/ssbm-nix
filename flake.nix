bf7063aac4169a3021913cfdebdaa320553419c6
Bjarki B. Harksen <bjarki@harksen.is>
2025-07-09 21:59:25 +0000

    use stable nixpkgs release

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     slippi-desktop.url = "github:project-slippi/slippi-desktop-app";
     slippi-desktop.flake = false;
   };
